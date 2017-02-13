07d3880cacfd9f5f9bfbe7a04c1ffee96b210956
jmiramant <josh@miramant.me>
2017-02-13 13:50:01 -0700

    syntax fix

--- controller.rb
+++ controller.rb
@@ -10,8 +10,8 @@ end
 post '/auth/images' do
 	# @user = User.findById(params['username'])
 	# @image_url = Picture.generateLoginPictures(@user.auth_image_ids);
-@user =  123
-@image_url = []
+	@user =  123
+	@image_url = []
 
 	9.times do |i|
 		@image_url.push('/cube.png')
